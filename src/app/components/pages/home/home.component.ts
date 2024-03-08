303b32e279e0112a9b00cb59bfaf26d5d638605d
Amir Hossein <am.h.sattari@gmail.com>
2024-03-08 16:21:07 +0330

    tags implemented

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -16,6 +16,8 @@ export class HomeComponent {
     route.params.subscribe((params) => {
       if(params.searchTerm)
         this.foods = foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else if (params.tag)
+        this.foods = foodService.getAllFoodsByTag(params.tag);
       else
       this.foods = foodService.getAll();
     })
