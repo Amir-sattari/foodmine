9f95a42c6a2f9c5305fde926d8eb8fe9eb37c09d
Amir Hossein <am.h.sattari@gmail.com>
2024-03-09 17:33:46 +0330

    the front of project almost finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { StarRatingComponent } from './components/partials/star-rating/star-rati
 import { SearchComponent } from './components/partials/search/search.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { TagsComponent } from './components/partials/tags/tags.component';
     StarRatingComponent,
     SearchComponent,
     FoodPageComponent,
-    TagsComponent
+    TagsComponent,
+    CartPageComponent,
+    TitleComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/pages/food-page/food-page.component.ts
+++ src/app/components/pages/food-page/food-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/food';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FoodService } from '../../../services/food.service';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-food-page',
@@ -11,11 +12,21 @@ import { FoodService } from '../../../services/food.service';
 export class FoodPageComponent {
   food!: Food;
 
-  constructor (route: ActivatedRoute, foodService: FoodService)
-  {
+  constructor(
+    route: ActivatedRoute,
+    foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
+  ) {
     route.params.subscribe((params) => {
-      if(params.id)
+      if (params.id)
         this.food = foodService.getFoodById(params.id);
     })
+  }
+
+  addToCart()
+  {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
   }
 }
